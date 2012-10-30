e8e2f1f0f511197d6f524563e987a3ec214a4d5b
luwenbin <luwenbin@live.com>
2012-10-30 21:44:24 +0800

    add slide

--- js/index.js
+++ js/index.js
@@ -22,27 +22,10 @@ $(function (){
 		 	$(".layFrame").css({"left":(cc-500)/2,"top":200})
 		 }
 	}
+	$("#slides").slides({
+        preload: true,play: 5000,pause: 2500,hoverPause: true,container: 'container', paginationClass: 'bannerNav',currentClass: 'bannerNavHover'
+    });
+    $(".bannerNav li a").html("")
 })
 
-var t = n = count = 0;
-$(function(){
-    count = $("#play_list a").size();   
-    $("#play_list a:not(:first-child)").hide();   
-    $("#play_text li:first-child").css({"background":"#fff",'color':'#000'});   
-    $("#play_info").click(function(){window.open($("#play_list a:first-child").attr('href'), "_blank")});   
-    $("#play_text li").click(function() {
-        var i = $(this).text() - 1;
-        n = i;
-        if (i >= count) return;   
-        $("#play_info").html($("#play_list a").eq(i).find("img").attr('alt'));   
-        $("#play_info").unbind().click(function(){window.open($("#play_list a").eq(i).attr('href'), "_blank")})   
-        $("#play_list a").filter(":visible").fadeOut(500).parent().children().eq(i).fadeIn(1000);
-        $(this).css({"background":"#fff",'color':'#000'}).siblings().css({"background":"#000",'color':'#fff'});   
-    });
-    t = setInterval("showAuto()", 6000);   
-    $("#play").hover(function(){clearInterval(t)}, function(){t = setInterval("showAuto()", 6000);});   
-})
-function showAuto(){
-    n = n >= (count - 1) ? 0 : n + 1;   
-    $("#play_text li").eq(n).trigger('click');   
-}
+
